5a27752d8028086c927c804927e490e9bf891f01
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-07-13 19:37:09 -0400

    trying another small update

--- endpoints.js
+++ endpoints.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Note = require('./models/note')
+const Note = require('./models/Note')
 
 /** Return all notes */
 router.get('/notes', (req, res, next) => {
